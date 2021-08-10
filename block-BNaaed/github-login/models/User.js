0987a5ebdf32b36b84112281e17312d0ba4f1441
sohailshaik8328 <sohailshaik765@gmail.com>
2021-08-10 10:15:52 +0530

    assn1

---
+++ block-BNaaed/github-login/models/User.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var passportUserSchema = new Schema({
+    name : String,
+    email : {type : String, required : true, unique : true},
+    username : {type : String, required : true, unique : true},
+    photo : {type : String}
+}, {timestamps : true});
+
+module.exports = mongoose.model('User', passportUserSchema);
